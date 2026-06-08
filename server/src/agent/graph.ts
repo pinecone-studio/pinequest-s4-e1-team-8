554f9ba0326475fdd49f67f535f046880937cf69
Danny Otgontsetseg <dnioko0412@gmail.com>
2026-06-08 14:19:45 +0800

    feat: implement onboarding worker node with feature generation state

--- server/src/agent/graph.ts
+++ server/src/agent/graph.ts
@@ -1,4 +1,5 @@
 import { END, START, StateGraph } from "@langchain/langgraph";
+import { onboardingWorkerNode } from "./nodes/onboarding";
 import { SupervisorGraphState } from "./state";
 
 export const SUPERVISOR_NODE = "supervisor";
@@ -7,10 +8,6 @@ export const METRICS_WORKER_NODE = "metrics_worker";
 export const RISK_WORKER_NODE = "risk_worker";
 
 async function supervisorNode(_state: typeof SupervisorGraphState.State) {
-  return {};
-}
-
-async function onboardingWorkerNode(_state: typeof SupervisorGraphState.State) {
   return {};
 }
 
